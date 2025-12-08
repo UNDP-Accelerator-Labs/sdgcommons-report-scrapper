b8993d48157daff52221823c85a960098cdccee1
Adedapo Aderemi <adedapopaul@yahoo.com>
2025-12-08 22:31:56 +0000

    update

--- src/scraper/selenium_driver.py
+++ src/scraper/selenium_driver.py
@@ -41,7 +41,7 @@ def setup_selenium():
     download_dir = tempfile.mkdtemp(prefix="sdg_scraper_dl_")
 
     chrome_options = Options()
-    # chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
